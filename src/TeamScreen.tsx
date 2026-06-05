bda1d021592ea1a9a02455bb95d73a474eaa86d8
Futuret3chdev <jason.c@futuret3ch.com.au>
2026-06-05 22:05:54 +1000

    Add TeamScreen

---
+++ src/TeamScreen.tsx
@@ -0,0 +1,9 @@
+import React from 'react';
+
+const TeamScreen = () => (
+  <div className="space-y-4">
+    <h1 className="text-4xl font-bold">Team Dominos</h1>
+    <div>Life Requests & Card Requests - Matching screenshots</div>
+  </div>
+);
+export default TeamScreen;
