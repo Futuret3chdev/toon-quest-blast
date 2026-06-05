5e180edad3eca276a9751afc0bc55eee719ece8d
Futuret3chdev <jason.c@futuret3ch.com.au>
2026-06-05 22:05:56 +1000

    Add LeaderboardScreen

---
+++ src/LeaderboardScreen.tsx
@@ -0,0 +1,9 @@
+import React from 'react';
+
+const LeaderboardScreen = () => (
+  <div>
+    <h1 className="text-4xl font-bold text-center">Weekly Leaderboard</h1>
+    <div className="mt-6">Podium with Haruki #1 etc. Full rankings coming</div>
+  </div>
+);
+export default LeaderboardScreen;
